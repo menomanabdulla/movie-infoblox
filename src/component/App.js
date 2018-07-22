6ea0566759f11bb90ebb53e52a5637fdf704d373
menomanabdulla <noman.fcibappy@gmail.com>
2018-07-22 17:54:57 +0600

    view model implement

--- src/component/App.js
+++ src/component/App.js
@@ -1,33 +1,78 @@
 import React, { Component } from 'react';
 import './App.css';
+import View from './view/view'
 
 class App extends Component {
-  movie=[
-    {
-      name: 'World War',
-      catagorie: 'milatary',
-      director: 'nolan',
-      ratting: 5,
-      thumb: 'http://haras.gov.ly/wp-content/uploads/2017/01/Logo.png'
-    },
-    {
-      name: 'World War two',
-      catagorie: 'milatary two',
-      director: 'nolan two',
-      ratting: 4,
-      thumb: 'http://haras.gov.ly/wp-content/uploads/2017/01/Logo.png'
-    }
-  ]
+  constructor (){
+    super()
+    this.movie=[
+      {
+        name: 'Man of Steel',
+        catagorie: 'Comics',
+        director: 'Zack Snyder',
+        ratting: 5,
+        thumb: 'https://i.pinimg.com/originals/b0/2e/e9/b02ee90794c0552f911532afac147300.jpg'
+      },{
+        name: 'Out Lander',
+        catagorie: 'Milatary',
+        director: 'Zim Cavizel',
+        ratting: 4,
+        thumb: 'https://i.pinimg.com/originals/a4/51/18/a451189f12689545ffb5141d606903f8.jpg'
+      },{
+        name: 'Beauty Kid',
+        catagorie: 'Horror',
+        director: 'Cristoper Liam',
+        ratting: 5,
+        thumb: 'https://i.pinimg.com/originals/0e/75/a5/0e75a5bcadec9c3463db9ae450012bcc.jpg'
+      },{
+        name: 'Blind Love',
+        catagorie: 'Romantic',
+        director: 'Jemy Heil',
+        ratting: 3,
+        thumb: 'https://i.pinimg.com/originals/77/83/d7/7783d7cb69830d7adddb8d3b6dc526a9.jpg'
+      },{
+        name: 'Ligting Baby',
+        catagorie: 'Horror',
+        director: 'Cristoper Liam',
+        ratting: 4,
+        thumb: 'https://i.pinimg.com/originals/4a/fc/6f/4afc6f3d14881f4c67370033d3146aed.jpg'
+      },{
+        name: 'Star War',
+        catagorie: 'Action',
+        director: 'Nolan Liam',
+        ratting: 5,
+        thumb: 'https://i.pinimg.com/originals/a0/3c/4b/a03c4b4d6c8702586f2aa67aecc744e1.jpg'
+      },{
+        name: 'False Stem',
+        catagorie: 'Action',
+        director: 'Nolan Liam',
+        ratting: 4,
+        thumb: 'https://i.pinimg.com/originals/bb/e3/73/bbe3736ca3522ff81849d90ad89b7031.jpg'
+      }
+    ]
+  }
+  
   render(){
     return (
       <div className="container">
-          <div className="offset-md-2 col-md-8">
-            <div className="movie-block">
-              <h1>Hello world from movie block</h1>
+        <div className="row">
+          <div className="col-md-9">
+              <div className="movie-view-block">
+                {
+                  this.movie.map((item,index) => {
+                    return <View key = {index} movie = {item} />
+                  })
+                }
+              </div>
             </div>
-          </div>
+            <div className="col-md-3">
+              <div className="movie-controller-block">
+
+              </div>
+            </div>
+        </div>
       </div>
-    );
+    )
   }
 }
 
--- src/component/view/view.js
+++ src/component/view/view.js
@@ -1,10 +1,46 @@
 import React ,{Component} from 'react'
 import './view.css'
+import ReactHtmlParser, { processNodes, convertNodeToElement, htmlparser2 } from 'react-html-parser'
 class View extends Component{
+    
+    ratting(){
+        let length = this.props.movie.ratting,i=0
+        let star = '<i class="fas fa-star"></i>'
+        let ratting = []
+        while(i<length){
+            ratting.push(star)
+            i++;
+        }
+        
+        let result = (
+            ratting.toLocaleString()
+        )
+        console.log(result)
+        //console.log( ReactHtmlParser(result))
+        
+        return  ReactHtmlParser(result)
+        
+
+    }
     render(){
         return(
-            <div>
+            <div className="single-movie">
+                <div className="movie-info-block">
+                    <div className="block-inner">
+                        <h4>{this.props.movie.name}</h4>
+                        <p>{this.props.movie.catagorie}</p>
+                        <p>{this.props.movie.director}</p>
+                    </div>
+                </div>
+                <div className="movie-ratting-block">
                 
+                   {
+                       this.ratting()
+                   }
+                </div>
+                <div className="movie-thumb-block">
+                    <img src={this.props.movie.thumb} alt='img'/>
+                </div>
             </div>
         )
     }
