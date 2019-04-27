05896eda73f820ab2a9ecdb8247b69007fab472f
menomanabdulla <noman.fcibappy@gmail.com>
2019-04-28 01:56:27 +0600

    updating navigation bar

--- src/component/App.js
+++ src/component/App.js
@@ -125,15 +125,48 @@ showLess(){
 
   render(){
     return (
-      <div className="container">
-        <div className="row">
-          <div className="col-md-12">
-              <div className="movie-view-block">
-                <View EditHandeler = {this.UpdateMoveList.bind(this)} 
-                  DeliteHendeler = {this.deleteMovie.bind(this)} 
-                  movie = {this.state.movie} />
+      <div>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light mb-3">
+          <a className="navbar-brand" href="#">MovieLog</a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ml-auto my-2 my-lg-0">
+              <li className="nav-item active">
+                <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Link</a>
+              </li>
+              <li className="nav-item dropdown">
+                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  Dropdown
+                </a>
+                <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <a className="dropdown-item" href="#">Action</a>
+                  <a className="dropdown-item" href="#">Another action</a>
+                  <div className="dropdown-divider"></div>
+                  <a className="dropdown-item" href="#">Something else here</a>
+                </div>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link disabled" href="#">Disabled</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+                <div className="movie-view-block">
+                  <View EditHandeler = {this.UpdateMoveList.bind(this)} 
+                    DeliteHendeler = {this.deleteMovie.bind(this)} 
+                    movie = {this.state.movie} />
+                </div>
               </div>
-            </div>
+          </div>
         </div>
       </div>
     )
